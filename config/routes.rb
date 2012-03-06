aec716108d12df03198ce2695109b4b44d978097
jason1098 <jasonbushis@gmail.com>
2012-03-06 13:13:58 -0800

    added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 URLShortener::Application.routes.draw do
   devise_for :users
-
+  root :to => "home#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
